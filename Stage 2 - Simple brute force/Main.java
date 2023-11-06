cc6731b3498ea36715a30862bceb104677963f99
Combat47 <115223808+combat47@users.noreply.github.com>
2023-11-06 10:02:21 +0330

    Main.java

---
+++ Stage 2 - Simple brute force/Main.java
@@ -0,0 +1,29 @@
+package hacker;
+
+import java.util.ArrayDeque;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //initialize required variables
+        String ip_address;
+        int port;
+
+        //process arguments
+        ip_address = args[0];
+        port = Integer.parseInt(args[1]);
+
+
+        //establish ConnectionLink & create initial deque
+        ConnectionLink activeConnection = new ConnectionLink(ip_address, port);
+        ArrayDeque<String> testingDeque = new ArrayDeque<String>();
+        testingDeque.add("");
+
+        //run the bruteforcer & grab the password returned
+        SimpleBruteForcer attack = new SimpleBruteForcer(testingDeque, activeConnection);
+        String password = attack.BruteForce();
+
+        //print the password
+        System.out.println(password);
+    }
+}
